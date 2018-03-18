1b1a527e5512733c71821196f59d50f7a71b54dd
Dmitry <kykaraha@mail.ru>
2018-03-18 15:49:32 +0300

    Add The Discrete Fourier Transform (Spectral) Test

--- NistTests.py
+++ NistTests.py
@@ -1,5 +1,6 @@
 import math
 import scipy.special as spc
+import scipy.fftpack as sff
 import numpy as np
 from BinaryMatrix import BinaryMatrix
 
@@ -209,3 +210,34 @@ class NistTest():
             return p_val
         else:
             return -1.0
+
+    def spectral(self, bin_data: str):
+        """
+        Note that this description is taken from the NIST documentation [1]
+        [1] http://csrc.nist.gov/publications/nistpubs/800-22-rev1a/SP800-22rev1a.pdf
+        The focus of this test is the peak heights in the Discrete Fourier Transform of the sequence. The purpose of
+        this test is to detect periodic features (i.e., repetitive patterns that are near each other) in the tested
+        sequence that would indicate a deviation from the assumption of randomness. The intention is to detect whether
+        the number of peaks exceeding the 95 % threshold is significantly different than 5 %.
+        :param bin_data: a binary string
+        :return: the p-value from the test
+        """
+        n = len(bin_data)
+        plus_minus_one = []
+        for char in bin_data:
+            if char == '0':
+                plus_minus_one.append(-1)
+            elif char == '1':
+                plus_minus_one.append(1)
+        # Product discrete fourier transform of plus minus one
+        s = sff.fft(plus_minus_one)
+        modulus = np.abs(s[0:n // 2])
+        tau = np.sqrt(np.log(1 / 0.05) * n)
+        # Theoretical number of peaks
+        count_n0 = 0.95 * (n / 2)
+        # Count the number of actual peaks m > T
+        count_n1 = len(np.where(modulus < tau)[0])
+        # Calculate d and return the p value statistic
+        d = (count_n1 - count_n0) / np.sqrt(n * 0.95 * 0.05 / 4)
+        p_val = spc.erfc(abs(d) / np.sqrt(2))
+        return p_val
