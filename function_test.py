c5a9ce080c4d886a806d5946b0a668c384af6875
Dmitry <kykaraha@mail.ru>
2018-03-29 23:36:43 +0300

    Add Salsa20 hash function

--- function_test.py
+++ function_test.py
@@ -1,4 +1,5 @@
 # Test helper function for Salsa20 cipher
+import time
 
 def bsum(a, b, mode='hex'):
     # Available modes: int, hex
@@ -52,9 +53,40 @@ def doubleround(*args):
     # (x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15) =
     return rowround(columnround(args[0]))
 
-def littleendian(b0, b1, b2, b3):
+def littleendian(*args):
+    (b0, b1, b2, b3) = args[0]
     result = "0x{0:0{1}x}".format(b0 + 2**8*b1 + 2**16*b2 + 2**24*b3, 8)
     return result
+
+def littleendian_reverse(word, mode='hex'):
+    if mode=='int':
+        word = hex(word)
+    b0 = int(word[2:4], 16)
+    b1 = int(word[4:6], 16)
+    b2 = int(word[6:8], 16)
+    b3 = int(word[8:10], 16)
+    return [b0, b1, b2, b3]
+
+def salsa20(*args, times=1):
+    for _ in range(times):
+        if _ >= 1:
+            X = little_reverse
+        elif _ == 0:
+            X = args[0]
+        x = []
+        for i in range(16):
+            x.append(littleendian(X[4*i:4*i+4]))
+        z = x
+        for _ in range(10):
+            z = doubleround(z)
+
+        little_reverse = []
+        for i in range(16):
+            for value in reversed(littleendian_reverse(bsum(x[i], z[i]))):
+                little_reverse.append(value)
+
+    return little_reverse
+
 
 if __name__ == '__main__':
 
@@ -170,10 +202,41 @@ if __name__ == '__main__':
     print("#"*80)
     print("Littleendian:")
     print("littleendian(0, 0, 0, 0) = 0x00000000")
-    print("littleendian(0, 0, 0, 0) = {}".format(littleendian(0, 0, 0, 0)))
+    print("littleendian(0, 0, 0, 0) = {}".format(littleendian([0, 0, 0, 0])))
     print("littleendian(86, 75, 30, 9) = 0x091e4b56")
-    print("littleendian(86, 75, 30, 9) = {}".format(littleendian(86, 75, 30, 9)))
+    print("littleendian(86, 75, 30, 9) = {}".format(littleendian([86, 75, 30, 9])))
     print("littleendian(255, 255, 255, 250) = 0xfaffffff")
-    print("littleendian(255, 255, 255, 250) = {}".format(littleendian(255, 255, 255, 250)))
+    print("littleendian(255, 255, 255, 250) = {}".format(littleendian([255, 255, 255, 250])))
 
+    print("#"*80)
+    print("Salsa20:")
+    print([109, 42,178,168,156,240,248,238,168,196,190,203, 26,110,170,154,
+       29, 29,150, 26,150, 30,235,249,190,163,251, 48, 69,144, 51, 57,
+       118, 40,152,157,180, 57, 27, 94,107, 42,236, 35, 27,111,114,114,
+       219,236,232,135,111,155,110, 18, 24,232, 95,158,179, 19, 48,202])
+    print(salsa20([211,159, 13,115, 76, 55, 82,183, 3,117,222, 37,191,187,234,136,
+                          49,237,179, 48, 1,106,178,219,175,199,166, 48, 86, 16,179,207,
+                          31,240, 32, 63, 15, 83, 93,161,116,147, 48,113,238, 55,204, 36,
+                          79,201,235, 79, 3, 81,156, 47,203, 26,244,243, 88,118,104, 54]))
+    print()
+    print([179, 19, 48,202,219,236,232,135,111,155,110, 18, 24,232, 95,158,
+           26,110,170,154,109, 42,178,168,156,240,248,238,168,196,190,203,
+           69,144, 51, 57, 29, 29,150, 26,150, 30,235,249,190,163,251, 48,
+           27,111,114,114,118, 40,152,157,180, 57, 27, 94,107, 42,236, 35])
+    print(salsa20([88,118,104, 54, 79,201,235, 79, 3, 81,156, 47,203, 26,244,243,
+                   191,187,234,136,211,159, 13,115, 76, 55, 82,183, 3,117,222, 37,
+                   86, 16,179,207, 49,237,179, 48, 1,106,178,219,175,199,166, 48,
+                   238, 55,204, 36, 31,240, 32, 63, 15, 83, 93,161,116,147, 48,113]))
 
+    # print()
+    # start = time.time()
+    # print([8, 18, 38,199,119, 76,215, 67,173,127,144,162,103,212,176,217,
+    #        192, 19,233, 33,159,197,154,160,128,243,219, 65,171,136,135,225,
+    #        123, 11, 68, 86,237, 82, 20,155,133,189, 9, 83,167,116,194, 78,
+    #        122,127,195,185,185,204,188, 90,245, 9,183,248,226, 85,245,104])
+    # print(salsa20([6,124, 83,146, 38,191, 9, 50, 4,161, 47,222,122,182,223,185,
+    #                75, 27, 0,216, 16,122, 7, 89,162,104,101,147,213, 21, 54, 95,
+    #                225,253,139,176,105,132, 23,116, 76, 41,176,207,221, 34,157,108,
+    #                94, 94, 99, 52, 90,117, 91,220,146,190,239,143,196,176,130,186], times=1000000))
+    # end = time.time()
+    # print(end - start)
